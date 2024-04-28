3a0c78df41eb0c33c37f5bd0629555de71a676c0
Hiro Cardenas <hiromar2015@gmail.com>
2024-04-27 23:11:48 -0500

    UPDATE: .properties to .yml

--- src/main/java/org/arquitech/authservice/Authentication/api/rest/AuthController.java
+++ src/main/java/org/arquitech/authservice/Authentication/api/rest/AuthController.java
@@ -2,16 +2,14 @@ package org.arquitech.authservice.Authentication.api.rest;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.arquitech.authservice.Authentication.domain.model.entity.User;
 import org.arquitech.authservice.Authentication.resource.AuthResponse;
 import org.arquitech.authservice.Authentication.resource.ChangePasswordRequest;
 import org.arquitech.authservice.Authentication.resource.LoginRequest;
 import org.arquitech.authservice.Authentication.resource.RegisterRequest;
 import org.arquitech.authservice.Authentication.service.AuthService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "auth", description = "the auth API")
 @RestController
